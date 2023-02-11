79e1bae6de4bcd6a1ea4f82735fee632108f623e
Guilherme Carvalho <guilherme.carvalhos@outlook.com>
2023-02-11 20:15:57 -0300

    Enviroments

--- gatitobook/src/app/animais/animais.ts
+++ gatitobook/src/app/animais/animais.ts
@@ -1,2 +1,13 @@
-export interface Animais {
+export interface Animal {
+  id: number;
+  postDate: Date;
+  url: string;
+  description: string;
+  allowComments: boolean;
+  likes: number;
+  comments: number;
+  userId: number;
 }
+
+export type Animais = Array<Animal>;
+
